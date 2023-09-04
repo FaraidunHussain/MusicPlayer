ef00e4bc2e5dc6284de5fdc1f3ca7c3f5108ab5b
Faraidun Hussain <133172907+FaraidunHussain@users.noreply.github.com>
2023-09-05 01:54:14 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const music_list = [
+    {
+        img : 'images/hassazirak.jpg',
+        name : 'Dktor',
+        artist : 'Hassan zirak',
+        music : 'music/zirak.mp3'
+    },
+    {
+        img : 'images/hassazirak.jpg',
+        name : 'Mastm',
+        artist : 'Hassan zirak',
+        music : 'music/mastm.mp3'
+    },
+      {
+         img : 'images/hassazirak.jpg',
+        name : 'katana',
+        artist : 'Hassan zirak',
+        music : 'music/katana.mp3'
+    }
+];
